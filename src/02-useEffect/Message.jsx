ad6cfa0c09ec3815bfcca07888a1a60e262877c8
Daniel Mateu <danielmateu86@gmail.com>
2023-03-14 09:50:46 +0100

    Limpieza del useEffect

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({ x: 0, y: 0 })
+
     useEffect(() => {
-        console.log('Componente montado')
+        // console.log('Componente montado')
+        const onMouseMove = ({ x, y }) => {
+            const coords = { x, y }
+            // console.log(coords)
+            setCoords(coords)
+        }
+
+        window.addEventListener('mousemove', onMouseMove)
+        
         return () => {
-            console.log('Componente desmontado')
+            // console.log('Componente desmontado')
+            window.removeEventListener('mousemove', onMouseMove)
         }
     }, [])
-    
+
     return (
         <>
             <h3>El usuario ya existe</h3>
+            {
+                coords && <p>{JSON.stringify(coords)}</p>
+            }
         </>
     )
 }
