d1ec537731de448aad2d4228ec5c16ec9dfac1ac
Daniel Mateu <danielmateu86@gmail.com>
2023-03-14 17:17:44 +0100

    Tarea memorize

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import { Layout } from './05-useLayoutEffect/Layout'
 import { CallbackHook } from './06-memos/CallbackHook'
 import { MemoHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
+import { Padre } from './07-tarea-memo/Padre'
 import { HooksApp } from './HooksApp'
 import './index.css'
 
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     // <Layout/>
     // <Memorize/>
     // <MemoHook/>
-    <CallbackHook/>
+    // <CallbackHook/>
+    <Padre/>
 
 )
