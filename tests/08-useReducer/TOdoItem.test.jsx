cc72451b528d450b9d3902704dc0368b8a137890
Daniel Mateu <danielmateu86@gmail.com>
2023-03-17 14:29:51 +0100

    Tests en todoItem

--- tests/08-useReducer/TOdoItem.test.jsx
+++ tests/08-useReducer/TOdoItem.test.jsx
@@ -25,7 +25,7 @@ describe('Pruebas sobre TOdoItem', () => {
         const liElement = screen.getByRole('listitem')
         // console.log(liElement)
         expect(liElement.className).toBe('list-group-item d-flex justify-content-between align-items-center')
-        
+
         const spanElement = screen.getByText(todo.description)
         expect(spanElement.className).toContain('')
         expect(spanElement.tagName).toBe('SPAN')
@@ -34,4 +34,57 @@ describe('Pruebas sobre TOdoItem', () => {
         expect(handleToggle).not.toHaveBeenCalled()
 
     })
+
+    test('Debe de mostrarse el Todo completado', () => {
+
+        const todo = {
+            id: 1,
+            description: 'Aprender React',
+            done: true
+        }
+
+        render(<TOdoItem
+            todo={todo}
+            handleDeleteTodo={handleDeleteTodo}
+            handleToggle={handleToggle}
+        />)
+
+        const spanElement = screen.getByText(todo.description)
+        expect(spanElement.className).toContain('text-decoration-line-through')
+
+    })
+
+    test('el Span debe llamar el ToggleTodo cuando se hace click', () => {
+
+        render(<TOdoItem
+            todo={todo}
+            // handleDeleteTodo={handleDeleteTodo}
+            handleToggle={handleToggle}
+        />)
+
+        const spanElement = screen.getByText(todo.description)
+        spanElement.click()
+
+        expect(handleToggle).toHaveBeenCalled()
+        expect(handleToggle).toHaveBeenCalledWith(todo.id)
+
+    })
+
+    test('el Boton debe llamar el handleDeleteTodo cuando se hace click', () => {
+
+        render(<TOdoItem
+            todo={todo}
+            handleDeleteTodo={handleDeleteTodo}
+        // handleToggle={handleToggle}
+        />)
+
+        const btnElement = screen.getByRole('button')
+        btnElement.click()
+
+        expect(handleDeleteTodo).toHaveBeenCalled()
+        expect(handleDeleteTodo).toHaveBeenCalledWith(todo.id)
+
+    })
+
+    
 })
